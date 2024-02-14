04020a21424908c7e277559cacfe4433f7794020
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2024-02-14 12:39:46 -0600

    Update

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -44,8 +44,8 @@ export default function BaseLayout() {
             <Grid item>
                <Box component={'footer'} display={'flex'} flexDirection={'column'} alignItems={'center'}
                     py={'1.5rem'} sx={{opacity: 0.7}} width={'100%'}>
-                  <p>From Costa Rica with 🤍 by <a href={'https://www.linkedin.com/in/rusbenmadrigal/'}>Russ Madrigal</a></p>
-                  <p>&copy; 2023</p>
+                  <p>From Costa Rica with 💚 by <a href={'https://www.linkedin.com/in/rusbenmadrigal/'}>Russ Madrigal</a></p>
+                  <p>&copy; 2024</p>
                </Box>
             </Grid>
          </Grid>
